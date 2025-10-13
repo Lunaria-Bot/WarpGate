9d68f3e00cd7066d537a8b44c0601866806c8c25
StarStream <alexiswiesen@yahoo.com>
2025-10-13 15:25:53 +0200

    Create db.py

---
+++ db.py
@@ -0,0 +1,27 @@
+# db.py
+import asyncpg
+from contextlib import asynccontextmanager
+from config import settings
+
+_pool = None
+
+async def init_db():
+    global _pool
+    _pool = await asyncpg.create_pool(dsn=settings.PG_DSN, min_size=1, max_size=10)
+
+def pool():
+    return _pool
+
+@asynccontextmanager
+async def tx():
+    conn = await _pool.acquire()
+    try:
+        tr = conn.transaction()
+        await tr.start()
+        yield conn
+        await tr.commit()
+    except:
+        await tr.rollback()
+        raise
+    finally:
+        await _pool.release(conn)
