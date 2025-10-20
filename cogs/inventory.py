1071f26da19b6844b225e6193070f120dc1d5e05
StarStream <alexiswiesen@yahoo.com>
2025-10-20 18:05:50 +0200

    Update inventory.py

--- cogs/inventory.py
+++ cogs/inventory.py
@@ -189,7 +189,7 @@ class Inventory(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command(name="inventory")
+    @commands.command(name="inventory", aliases=["inv"])
     async def inventory(self, ctx, member: Optional[discord.Member] = None):
         user = member or ctx.author
         user_id = int(user.id)
